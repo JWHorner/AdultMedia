d42d8bdac918cf165fc94b68a472f1855beb65f1
John Horner <john.horner@outlook.com>
2024-02-10 23:36:10 +0000

    Initial commit

---
+++ AdultMedia/media/lua/client/RecordedMedia/recorded_media.lua
@@ -0,0 +1,25 @@
+-- Generated Recorded Media Data File
+RecMedia = RecMedia or {}
+
+-- Media: VHS: Debbie Does Dallas [id: debbieDoesDallas]
+RecMedia["debbieDoesDallas"] = {
+	itemDisplayName = "VHS: Debbie Does Dallas",
+	title = "RM_7838d5f4-aa48-4ba0-b21e-7bc80ca885f8",
+	subtitle = "RM_835c1ec5-7e46-4ebd-b981-9363ac825423",
+	author = "RM_a4655334-f64c-46e8-8395-c617c610d998",
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_ec249753-8b5d-4706-a9d9-be2abef3b6e8", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_44380153-4e51-4702-8c6b-e9d744d8da9f", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_47dd8a69-f0bf-46e0-b27b-cbe45fdd1907", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_bb658d38-502d-42eb-bc8f-f672023cbf7b", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_42c34e31-63b0-4c15-a21b-7525cf31de56", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_acce207a-103b-4269-8598-cb29942f6900", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_88336ad2-dc05-4dac-9e44-629f715e77be", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_92161346-17b0-47b3-b149-be23b82f027f", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_8bc82ec2-9bae-42a0-bbed-b003207da7bd", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_46120309-ab5a-44ac-ac14-4e2da8734c28", r = 0.00, g = 0.00, b = 0.00, codes = "BOR-3" },
+	},
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+# Project Zomboid Adult Media Mod
+
+A Project Zomboid mod that adds Playboy, Hustler & Juggs adult magazines, and Debbie Does Dallas, Deep Throat & Emanuelle VHS tapes.
+
+## Usage
+
+Extract the contents of the Mod folder to `C:\Users\USERNAME\Zomboid\mods\`
+
+if done correctly it should look like:
+
+```
+C:\Users
+    +--USERNAME
+        +--Zomboid
+            +--mods
+                +--AdultMedia
+                    |--\Media
+                    |--mod.info
+			        +--poster.png
+```
