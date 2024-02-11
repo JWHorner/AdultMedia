b2300dc2a7c712840f4a409bbaeb58b34b616b22
John Horner <john.horner@outlook.com>
2024-02-11 00:14:42 +0000

    Deep throat & Emmanuelle finished

--- AdultMedia/media/lua/client/RecordedMedia/recorded_media.lua
+++ AdultMedia/media/lua/client/RecordedMedia/recorded_media.lua
@@ -23,3 +23,49 @@ RecMedia["debbieDoesDallas"] = {
 		{ text = "RM_46120309-ab5a-44ac-ac14-4e2da8734c28", r = 0.00, g = 0.00, b = 0.00, codes = "BOR-3" },
 	},
 };
+
+-- Media: VHS: Deep Throat [id: deepThroat]
+RecMedia["deepThroat"] = {
+	itemDisplayName = "VHS: Deep Throat",
+	title = "RM_e8bce0b5-a1cd-4925-b417-2e7419ff89af",
+	subtitle = "RM_d37947c5-fd41-42a3-b96c-ca8709921887",
+	author = "RM_34583fe8-d53a-4200-83ca-72ef969c0fb6",
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_079e18bd-8303-4afb-aa7a-89bd7d304180", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_a3b0a558-7d26-4498-8730-fe4fd6358e11", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_99db01b8-5c5e-46e4-8146-dac2179a6c10", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_99b300d0-8804-4abd-9d4e-7654ee191b23", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_d22df53c-e6c3-44f6-878b-620749b2e5d8", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_4e149173-7ef9-4969-8667-ca15659c9d3f", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_23ee90f8-8151-4202-a51c-91e9ec3326c6", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_13070579-bed7-4986-b2ae-fbb2b3ed78b1", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_544c70d0-e631-4566-8f62-7e1214ee8a68", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_9486f7b9-40c0-443d-8309-ebfc7606c82e", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+	},
+};
+
+-- Media: VHS: Emmanuelle [id: emmanuelle]
+RecMedia["emmanuelle"] = {
+	itemDisplayName = "VHS: Emmanuelle",
+	title = "RM_8a395481-a866-486e-9bd0-cb121b6736d4",
+	subtitle = "RM_8576c127-f7c2-4013-b43b-85bf23fe3ed6",
+	author = "RM_84696afa-6281-4639-ba3b-630bcef71e9f",
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_4a005581-f712-4e77-963d-58048cf5bf12", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_7f8d8e3f-f84e-40a5-a465-e2115a162506", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_6bd03a7a-28de-4beb-aa9c-ae3cf21db1d8", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_5d0d25e4-68c6-45d4-a689-98dd0e4c9840", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_336babda-4bc1-483b-abd0-918a07644622", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_1f6a1275-5e90-4aab-bcc0-69d0a43ebdeb", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_0338e7c9-52eb-4355-9b44-c9c0f8dd9c90", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_1900d60c-41fc-4869-beef-f2beb1148729", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_323e6d6c-58b5-46c6-ab90-e1d4a49e56ec", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+		{ text = "RM_46367a22-a5da-4c25-b57f-87f291612cfd", r = 0.40, g = 0.40, b = 0.40, codes = "BOR-3" },
+	},
+};
